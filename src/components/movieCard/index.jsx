7e5cca5487d674af18e76e872985aaef15aecd55
Conor <conormomalley@gmail.com>
2023-06-19 21:51:48 +0100

    Bug Fix - make action icons on movie card configurable.

--- src/components/movieCard/index.jsx
+++ src/components/movieCard/index.jsx
@@ -10,7 +10,6 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
 import StarRateIcon from "@mui/icons-material/StarRate";
 import Grid from "@mui/material/Grid";
-import IconButton from "@mui/material/IconButton";
 import img from '../../images/film-poster-placeholder.png'
 import { Link } from "react-router-dom";
 import Avatar from "@mui/material/Avatar";
@@ -26,7 +25,8 @@ const styles = {
   },
 };
 
-export default function MovieCard({ movie }) {      // Destructure props
+export default function MovieCard({ movie, action }) {
+  // Destructure props
   const { favourites, addToFavourites } = useContext(MoviesContext);
 
   if (favourites.find((id) => id === movie.id)) {
@@ -35,10 +35,7 @@ export default function MovieCard({ movie }) {      // Destructure props
     movie.favourite = false
   }
 
-  const handleAddToFavourite = (e) => {
-    e.preventDefault();
-    addToFavourites(movie);
-  };
+  
 
 
   return (
@@ -83,9 +80,8 @@ export default function MovieCard({ movie }) {      // Destructure props
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-            <IconButton aria-label="add to favourites" onClick={handleAddToFavourite}>
-        <FavoriteIcon color="primary" fontSize="large" />
-    </IconButton>
+      {action(movie)}
+
 
         <Link to={`/movies/${movie.id}`}>
 
